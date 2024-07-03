29e303aace399c744685123d7b678c63866b74f6
Lucas Pinheiro <lucaspinheiro@Lucass-MacBook-Air.local>
2024-07-02 21:45:58 -0300

    knsdkfb

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
-from flask import Flask, request, jsonify, redirect, url_for, render_template, session
+from flask import Flask, request, jsonify, render_template
 from werkzeug.utils import secure_filename
-from sqlalchemy.exc import IntegrityError
-import cloudinary.uploader
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone, timedelta
+import cloudinary.uploader
 import os
 
 app = Flask(__name__)
+app.secret_key = 'uma_chave_secreta_muito_segura'
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 * 1024  # 1 GB
 
-uri = os.getenv(
-    "DATABASE_URL", "sqlite:///local.db"
-)  # Default to SQLite for local development
+uri = os.getenv("DATABASE_URL", "sqlite:///local.db")
 if uri.startswith("postgres://"):
-    uri = uri.replace(
-        "postgres://", "postgresql://", 1
-    )  # Required for SQLAlchemy compatibility
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
 
 cloudinary.config(
     cloud_name="hbmghdcte",
@@ -26,8 +23,6 @@ cloudinary.config(
 
 db = SQLAlchemy(app)
 
-
-# Definição do modelo da tabela
 class FormEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
@@ -42,67 +37,64 @@ class FormEntry(db.Model):
     promocao = db.Column(db.String(100), nullable=True)
     assistencia = db.Column(db.String(100), nullable=True)
     link_arquivo = db.Column(db.String(200), nullable=True)
+    aceite_termos = db.Column(db.String(200), nullable=True)
+    data_envio = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
 
-# Cria a tabela no banco de dados
 with app.app_context():
     db.create_all()
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        session["form_data"] = request.form.to_dict()
-        session["video_file"] = request.files["videoUpload"]
-        return redirect(url_for("termo_aceite"))
+        form_data = request.form.to_dict()
 
-    return render_template("form.html")
-
-
-@app.route("/termo_aceite", methods=["GET", "POST"])
-def termo_aceite():
-    if request.method == "POST":
-        if "aceito" in request.form:
+        # Upload de vídeo
+        video_file = request.files.get("videoUpload")
+        if video_file:
             try:
-                video_file = session["video_file"]
+                # Upload to Cloudinary
                 upload_result = cloudinary.uploader.upload(
-                    video_file, resource_type="video"
+                    video_file,
+                    resource_type='video',
+                    folder='video_uploads'
                 )
-                video_url = upload_result["url"]
+                link_video = upload_result.get('url')
+                
+                bsb_time = datetime.now(timezone(timedelta(hours=-3)))
 
-                new_entry = FormEntry(**session["form_data"], link_arquivo=video_url)
+                # Salvando no banco de dados
+                new_entry = FormEntry(
+                    nome=form_data.get('nome'), 
+                    cpf=form_data.get('cpf'), 
+                    endereco=form_data.get('endereco'), 
+                    cep=form_data.get('cep'),
+                    estado=form_data.get('estado'), 
+                    telefone=form_data.get('telefone'), 
+                    tema=form_data.get('tema'),
+                    referencia_video=form_data.get('referencia_video'),
+                    promocao=form_data.get('promocao'),
+                    formacao=form_data.get('formacao'),
+                    assistencia=form_data.get('assistencia'),
+                    aceite_termos=form_data.get('privacyPolicy'),
+                    link_arquivo=link_video,
+                    data_envio=bsb_time
+                )
                 db.session.add(new_entry)
                 db.session.commit()
 
-                # Limpar a sessão após o uso
-                session.pop("form_data", None)
-                session.pop("video_file", None)
-
-                return (
-                    jsonify(
-                        {
-                            "message": "Vídeo registrado com sucesso!",
-                            "video_url": video_url,
-                        }
-                    ),
-                    200,
-                )
+                return jsonify({"message": "Vídeo registrado com sucesso!", "link_arquivo": link_video})
             except Exception as e:
                 return jsonify({"error": str(e)}), 500
         else:
-            return render_template(
-                "termo_aceite.html", error="Você deve aceitar os termos para continuar."
-            )
+            return jsonify({"error": "Arquivo de vídeo é obrigatório."}), 400
 
-    form_data = session.get("form_data", {})
-    return render_template("termo_aceite.html", form_data=form_data)
-
+    return render_template("form.html")
 
 @app.route("/entries")
 def view_entries():
     entries = FormEntry.query.all()
     return render_template("entries.html", entries=entries)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
